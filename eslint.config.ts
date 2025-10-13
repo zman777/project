2c5ebec6231025922ec6f79b9ddfc761ec1f97df
zman777 <95173136+zman777@users.noreply.github.com>
2025-10-13 19:15:43 +0800

    feat: 初始化Vue3项目基础配置
    添加Vue3项目基础文件结构，包括:
    - 核心配置文件(tsconfig, vite, eslint, stylelint)
    - 基础目录结构(src, public)
    - 开发工具配置(prettier, husky)
    - 示例组件和入口文件

---
+++ eslint.config.ts
@@ -0,0 +1,61 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfig } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+  ...tseslint.configs.recommended,
+  pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+  prettier,
+  {
+    rules: {
+      // --- ESLint 核心规则 ---
+      'no-var': 'error', // 要求使用 let 或 const 而不是 var
+      'no-multiple-empty-lines': ['warn', { max: 1 }], // 不允许多个空行
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-unexpected-multiline': 'error', // 禁止空余的多行
+      'no-useless-escape': 'off', // 禁止不必要的转义字符
+
+      // --- TypeScript 规则 ---
+      '@typescript-eslint/no-unused-vars': 'error', // 禁止定义未使用的变量
+      '@typescript-eslint/prefer-ts-expect-error': 'error', // 推荐使用 @ts-expect-error 而非 @ts-ignore
+      '@typescript-eslint/no-explicit-any': 'off', // 允许使用 any 类型
+      '@typescript-eslint/no-non-null-assertion': 'off', // 允许使用非空断言 !
+      '@typescript-eslint/no-namespace': 'off', // 允许使用 namespace
+      '@typescript-eslint/semi': 'off', // 关闭强制使用分号的规则（通常交给 Prettier 处理）
+
+      // --- Vue 规则 ---
+      'vue/multi-word-component-names': 'off', // 允许单字组件名
+      'vue/no-mutating-props': 'off', // 允许修改 props（不推荐，但教程开了）
+      'vue/attribute-hyphenation': 'off', // 允许属性使用驼峰命名
+    },
+  },
+])
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>我是APP</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+for (let i = 0; i < 1000000000; i++) {
+  console.log(i);
+
+}
+</script>
+
+<style scoped></style>
