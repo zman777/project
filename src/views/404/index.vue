a5916c92cded03f2dd344aef6c91b1f38ea5f69a
zman777 <95173136+zman777@users.noreply.github.com>
2025-10-15 14:09:59 +0800

    style: 统一格式化多个vue文件的代码样式
    修复文件末尾缺少换行符的问题
    统一缩进为2个空格
    简化script标签的空内容写法

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,11 +1,9 @@
 <template>
-    <div class="">
-        <h1>我是一级路由404页面</h1>
-    </div>
+  <div class="">
+    <h1>我是一级路由404页面</h1>
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,9 @@
 <template>
-    <div class="">
-        <h1>我是一级路由展示登录成功以后的数据</h1>
-    </div>
+  <div class="">
+    <h1>我是一级路由展示登录成功以后的数据</h1>
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
